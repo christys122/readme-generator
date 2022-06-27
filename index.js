84df25fbcdba2c1909ff667c3a2b53ecece7de00
unknown <mcsalazar122@gmail.com>
2022-06-26 22:52:17 -0500

    question set up

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 // TODO: Include packages needed for this application
-
+//const { fs } = require('fs');
+//require('fs');
 require('inquirer');
-require('fs');
+
 // TODO: Create an array of questions for user input
-const questions = [];
+// orig = const questions = [];
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'fileName',
+            message: 'What is your file name? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Enter file name');
+                    return false;
+                }
+            }
+        },
+
+    ])
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -13,3 +32,9 @@ function init() {}
 
 // Function call to initialize app
 init();
+fs.writeFile('README.md', generatePage(fileName, data, err => {
+    if (err) throw err;
+    console.log(complete);
+}));
+const inquirer = require('inquirer');
+const generatePage = require('./utils/generateMarkdown.js');
